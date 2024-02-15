4b2e32a915147b8b2db580be497d4a3f908f1651
Sumaila Valentine Oshoke <152486776+faveyval@users.noreply.github.com>
2024-02-15 14:19:04 +0100

    Add files via upload
    upload files

---
+++ DOM/js/main.js
@@ -0,0 +1,10 @@
+//grab the mobile nav element
+let mobileNavElem = document.getElementById("mobile-nav");
+
+
+// hamburger display function
+
+function displayMobileNav() {
+    // console.log("PROF!!!!!");
+    mobileNavElem.classList.toggle("show");
+}
